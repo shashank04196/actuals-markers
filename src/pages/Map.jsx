453958f7d291a1b374597edcd701aa37c0063f08
shashank04196 <shashankpotharaveni@gmail.com>
2025-05-16 06:09:01 +0530

    Update Map.jsx

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,41 +1,49 @@
+
 import React, { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import Game from "./Game";
 
-function Map() {
-  useEffect(() => {
-    const map = L.map("map").setView([51.505, -0.09], 13);
+function Map({ onGuess } = {}) {
+    useEffect(() => {
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+        const map = L.map("map-guess").setView([0, 80], 3);
 
-    let currentMarker = null;
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map);
 
-    map.on("click", function (e) {
-      const { lat, lng } = e.latlng;
 
-      if (currentMarker) {
-        map.removeLayer(currentMarker);
-      }
+        let currentMarker = null;
 
-      currentMarker = L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(`Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}`)
-        .openPopup();
+        map.on("click", function(e) {
+            const { lat, lng } = e.latlng;
 
-      console.log("Selected coordinate:", { lat, lng });
-    });
+            if (currentMarker) {
+                map.removeLayer(currentMarker);
+            }
 
-    // Clean up map on component unmount
-    return () => {
-      map.remove();
-    };
-  }, []);
+            currentMarker = L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(`Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}`)
+                .openPopup();
 
-  return <div id="map" style={{ height: "100%", width: "100%" }}></div>;
+            onGuess({ lat, lng });
+
+            console.log("Selected coordinate:", { lat, lng });
+        });
+
+        // Clean up map on component unmount
+        return () => {
+            map.remove();
+        };
+    }, [onGuess]);
+
+    return <div id = "map-guess"
+    style = {
+        { height: "100%", width: "100%" }
+    } > < /div>;
 }
 
 export default Map;
