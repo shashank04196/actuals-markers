12d9252567f59c7635ae48f7f782e199c01352b9
shashank04196 <shashankpotharaveni@gmail.com>
2025-05-16 06:06:42 +0530

    Update Score.jsx

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,17 +1,32 @@
 import "./Score.css";
+import MapScore from "./MapScore";
+import { useLocation } from "react-router-dom";
+import { act } from "react";
 
 function Score() {
-  return (
-    <div className="dashboard-container">
-      <div className="top-row">
-        <div className="box">deviation</div>
-        <div className="box">score</div>
-      </div>
-      <div className="bottom-row">
-        <div className="box">high score + etc</div>
-      </div>
-    </div>
-  );
+    const location = useLocation();
+    const { guessCoords, actualCoords } = location.state || {};
+    return ( <
+        div className = "dashboard-container" >
+        <
+        div className = "top-row" >
+        <
+        div className = "box" >
+        <
+        MapScore guessCoords = { guessCoords }
+        actualCoords = {
+            actualCoords
+        }
+        / > < /
+        div > <
+        div className = "box" > score < /div> < /
+        div > <
+        div className = "bottom-row" >
+        <
+        div className = "box" > high score + etc < /div> < /
+        div > <
+        /div>
+    );
 }
 
 export default Score
